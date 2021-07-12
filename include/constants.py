4293d708269b8bc1300c39d4a6cfee2c3470441a
maddarauci <dominicd165@gmail.com>
2021-07-12 12:42:14 +1000

    first commit

---
+++ include/constants.py
@@ -0,0 +1,8 @@
+BASE_URL = "https://www.instagram.com/"
+LOGIN_URL = BASE_URL + 'accounts/login/ajax/'
+LOGOUT_URL = BASE_URL + 'accounts/logout/'
+CHROME_WIN_UA = 'Mozilla'
+USER_URL = BASE_URL = + '{0/}?__a=1'
+USER_INFO = 'https://i.instagram.com/api/v1/users/{0}/info/'
+
+# more need to go in the following lines.
